522debe3242178cfcf9c66f84713b0f84511ad15
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-17 20:14:24 +0000

    1

--- hello_world/1-last_digit.c
+++ hello_world/1-last_digit.c
@@ -9,9 +9,10 @@
 int main(void)
 {
 	int n;
-	int ld = n % 10;
+	int ld;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
 	if (n > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, ld);
