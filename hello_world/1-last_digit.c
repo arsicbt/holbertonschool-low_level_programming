edf1c7a9394ab8753a2aa4e235bad8e30bfee682
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-17 20:08:12 +0000

    1

---
+++ hello_world/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int ld = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, ld);
+        }
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, ld);
+	}
+	else
+	{
+	       printf("Last digit of %d is %d and is less than 6 and not 0", n, ld);
+	}
+	return (0);
+}
