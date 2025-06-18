429731dea0ee8cff1e7efe84b0b2348f316cf744
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-18 11:56:59 +0000

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-        char *alphabet = "abcdefghijklmnopqrstuvwxyz\0";
-        char a  = alphabet[0];
+	char *alphabet = "abcdefghijklmnopqrstuvwxyz\0";
+	char a  = alphabet[0];
 	char last_char = '{';
 
-        while (a != last_char)
-        {
+	while (a != last_char)
+	{
 		if (a != 'e' && a != 'q')
 		{
 			putchar(a);
 		}
-                a++;
-        }
-        putchar('\n');
-        return (0);
+		a++;
+	}
+	putchar('\n');
+	return (0);
 }
