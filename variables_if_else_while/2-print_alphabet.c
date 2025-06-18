b8c7e3ef58cb928c53a671454c1585da460982e4
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-18 08:59:01 +0000

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char *alphabet = "abcdefghijklmnopqrstuvwxyz\0";
+	char last_char = '{';
+	char a = alphabet[0];
+
+   	while (a != last_char)
+	{
+		putchar(a);
+		a++;
+	}
+        return (0);
+}
