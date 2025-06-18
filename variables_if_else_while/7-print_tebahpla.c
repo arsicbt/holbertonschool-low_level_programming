b2544ed63f8f81758fd1e4be071e2fdff361528b
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-18 17:31:49 +0000

    7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,23 @@
  */
 int main(void)
 {
+<<<<<<< HEAD
 	char *alphabet = "abcdefghijklmnopqrstuvwxyz";
 	char *tebahpla = alphabet;
 
 	while (tebahpla >= alphabet)
 	{
 		putchar(*tebahpla);
+=======
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', '\0'};
+	int tebahpla;
+
+	for (tebahpla = 25; tebahpla >= 0; tebahpla--)
+	{
+		putchar(alphabet[tebahpla]);
+>>>>>>> fc13bfa (7)
 	}
 	putchar('\n');
 	return (0);
