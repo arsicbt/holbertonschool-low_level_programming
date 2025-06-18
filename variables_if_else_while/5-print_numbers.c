db9689a19b46fe2eab00f7c22317c9118fc909bb
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-18 13:20:46 +0000

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,9 +6,13 @@
  */
 int main(void)
 {
-        int num = 0123456789;
+	char num[] = "0123456789";
+	int i = 0;
 
-        putchar(num);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(num[i]);
+	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
