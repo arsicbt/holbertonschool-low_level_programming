a9a522210fb3eeb543696dacb7aa179d9a83797a
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-20 13:18:21 +0000

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check if is lower.
+ * @c : index
+ * Return: 1 or 0.
+ */
+int _islower(char c)
+{
+	if (c >= 'a' && c >= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ functions_nested_loops/3-islower.cy
@@ -0,0 +1,19 @@
+#include
+/**
+ * main - check if c is lower.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+
+	if (c <= 'a' && c >= 'z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
