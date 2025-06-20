a12cb3019e4437e04ae460c0081d5dff46b677f4
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-20 11:36:42 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int print_alphabet(void)
 {
-	 char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-			'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-			'x', 'y', 'z', '\n'};
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+	'x', 'y', 'z', '\n'};
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 26; i++)
 	{
 		_putchar(alphabet[i]);
 	}
-	 return (0);
+	return (0);
 }
