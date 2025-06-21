f3a074474717125cb81ef2bbbc65734c87bc6d84
Yarsicbt <chobertarsinoe@gmail.com>
2025-06-21 16:16:24 +0000

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - find the last digit of n.
+ * @n : number
+ * Return: last digit.
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}
