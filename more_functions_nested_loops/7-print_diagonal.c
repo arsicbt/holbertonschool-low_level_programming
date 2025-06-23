b9ff1105ddcbbc06835501d93ef959dbc9f4aabc
arsicbt <chobertarsinoe@gmail.com>
2025-06-23 14:45:52 +0000

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ *
+ * Return: a diagonal line
+ */
+void print_diagonal(int n)
+{
+	char i;
+	char j;
+
+	if (n > 1)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
