eb81344466fd9142f707d23a2dc88fb3b6dbfe6a
arsicbt <chobertarsinoe@gmail.com>
2025-06-23 09:38:01 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ *@a: number 1
+ *@b : number 2
+ * Return: the multiplication of a and b.
+ */
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -14,5 +14,6 @@ void times_table(void);
 int add(int, int);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
