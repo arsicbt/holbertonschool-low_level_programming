a60a62a1513f9ac57a328b3fdd209192882e139d
arsicbt <chobertarsinoe@gmail.com>
2025-06-23 08:51:37 +0000

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,17 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 91)
+	if (c >= 65 && c < 91)
 	{
 		return (1);
+	}
+	else if (c >= 97 && c < 122)
+	{
+		return (0);
+
 	}
 	else
 	{
 		return (0);
-
 	}
 }
