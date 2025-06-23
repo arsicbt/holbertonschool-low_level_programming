97d1cc7f3ef2b5d5f001189ed9236c74f0050236
arsicbt <chobertarsinoe@gmail.com>
2025-06-23 12:30:50 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,20 @@
  */
 void more_numbers(void)
 {
-	int num_list[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, 1, 2, 1, 3, 1, 4};
+	int num_list[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 	int row;
 	int num;
 
 	for (row = 0; row < 10; row++)
 	{
-		for (num = 0; num < 20; num++)
+		for (num = 0; num < 15; num++)
 		{
-			_putchar(num_list[num] + '0');
+			if (num > 9)
+			{
+				_putchar((num_list[num] / 10) + '0');
+			}
+			_putchar((num_list[num] % 10) + '0');
 		}
-		_putchar('\n');
-	}
 	_putchar('\n');
+	}
 }
