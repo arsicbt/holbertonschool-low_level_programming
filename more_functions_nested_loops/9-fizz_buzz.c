06855ed7bd73226d056c043c730e47d2bcd118ca
arsicbt <chobertarsinoe@gmail.com>
2025-06-24 15:08:52 +0000

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		}
@@ -25,10 +29,6 @@ int main(void)
 		else
 		{
 			printf(" %d", i);
-		}
-		if (i == 1)
-		{
-			printf("%d", i);
 		}
 	}
 	printf("\n");
