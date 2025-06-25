eafee2b4fd71473ae59106b294555b674f14abfb
arsicbt <chobertarsinoe@gmail.com>
2025-06-25 11:45:52 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the len of an str.
+ * @s: char *
+ * Return: the len.
+ */
+int _strlen(char *s)
+{
+	int i;
+	int len;
+	char str = *s;
+
+	for (i = 0; i < str; i++)
+	{
+		len = i;
+	}
+	return (len);
+}
