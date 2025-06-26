723edbfa83241e548a9d36c3a060b8492f893c66
arsicbt <chobertarsinoe@gmail.com>
2025-06-26 07:05:21 +0000

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - updates the value it points.
+ * @str: string
+ * Return: 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
