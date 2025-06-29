7b5b765994acd0af14426d71eb7747b0e91fc27f
arsicbt <chobertarsinoe@gmail.com>
2025-06-29 15:38:09 +0000

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+**/
+char *string_toupper(char *)
+{
+	char str;
+	char * = str;
+	int element;
+
+	for (element = 0; str[element] != '\0'; element++)
+	{
+		if (c >= 65 && c <= 91)
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
+		_putchar('1');
+		_putchar('\n');
+		return (1);
+	}
+	else
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+
+	}
