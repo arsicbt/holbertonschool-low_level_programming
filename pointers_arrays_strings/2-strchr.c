fd9489be2f7707bbf0ef66d107fac8fe60a020b4
arsicbt <chobertarsinoe@gmail.com>
2025-06-30 12:26:54 +0000

    2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _strchr - Display the c's characteres, betwin c in a string
+ * @s: pointer to the start of of a string
+ * @c: charactere
+ * Return: NULL
+**/
+char *_strchr(char *s, char c)
+{
+	int charactere;
+
+	for (charactere = 0; s[charactere] != '\0'; charactere++)
+	{
+		if (s[charactere] == c)
+		{
+			return (&s[charactere]);
+		}
+	}
+	return (NULL);
+}
