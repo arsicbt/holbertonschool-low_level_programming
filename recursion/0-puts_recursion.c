df922408c469f011ebfdba666cc65d21a1425d49
arsicbt <chobertarsinoe@gmail.com>
2025-07-04 09:46:17 +0000

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: Pointer to the string to print
+ *
+ * Description: This function prints each character of the string
+ * recursively, followed by a newline character at the end.
+ *
+ * Return: Nothing (void)
+ */
+void _puts_recursion(char *s)
+{
+	_puts_recursion(s);
+}
