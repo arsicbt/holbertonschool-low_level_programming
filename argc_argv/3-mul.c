332d8bebaacee8d53dd9267c0f55bcd142ae58ac
arsicbt <chobertarsinoe@gmail.com>
2025-07-06 19:26:15 +0000

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers passed as command-line arguments
+ * @argc: Argument count
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int sum;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	sum = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", sum);
+	return (0);
+}
