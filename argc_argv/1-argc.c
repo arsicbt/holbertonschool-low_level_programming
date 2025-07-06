1bf3fc0685bf686581f5e79efa03b99ee74e2867
arsicbt <chobertarsinoe@gmail.com>
2025-07-06 18:02:12 +0000

    1

---
+++ argc_argv/1-argc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vector (unused)
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+        printf("%s\n", argc);
+        return (0);
+}
