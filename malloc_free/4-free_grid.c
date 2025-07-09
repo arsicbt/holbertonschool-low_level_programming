f5e8ad2fac5c9ec191ea4b281332e19b635258c9
arsicbt <chobertarsinoe@gmail.com>
2025-07-09 12:50:54 +0000

    4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 
 /**
-**/
+ * free_grid - Frees a 2D array (grid) previously created by malloc
+ * @grid: Pointer to the 2D array
+ * @height: Number of rows in the grid
+ *
+ * Description: Frees each row of the grid, then frees the grid itself.
+ * If grid is NULL, the function does nothing.
+ */
 void free_grid(int **grid, int height)
 {
 	int h;
