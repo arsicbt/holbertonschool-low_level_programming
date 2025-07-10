541486cb38d4c302e11a8332adc2222b42c164f0
arsicbt <chobertarsinoe@gmail.com>
2025-07-10 11:52:24 +0000

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * init_dog - Initialize a variable of type struct dog
@@ -10,6 +12,11 @@
 **/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		printf("Error : Any data");
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
