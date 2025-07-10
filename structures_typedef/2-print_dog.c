f3cfa3236aad62e00ba566d9fa2edb4518d0c434
arsicbt <chobertarsinoe@gmail.com>
2025-07-10 13:49:52 +0000

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -21,25 +21,16 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 	{
 		printf("Name : %s\n", nil);
-		free(d);
 	} else
 	{
 		printf("Name : %s\n", d->name);
 	}
 
-	if (d->age == 0)
-	{
-		printf("Age : %s\n", nil);
-		free(d);
-	} else
-	{
-		printf("Age : %f\n", d->age);
-	}
+	printf("Age : %f\n", d->age);
 
 	if (d->owner == NULL)
 	{
 		printf("Owner : %s\n", nil);
-		free(d);
 	} else
 	{
 		printf("Owner : %s\n", d->owner);
