afbf89117da55fa5e63da1633da8741249ee43c3
arsicbt <chobertarsinoe@gmail.com>
2025-07-29 09:01:47 +0000

    fix: Remove the tool fils (for intranet's compilation)

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "lists.h"
+
+/**
+ * _strlen - check the len of an str.
+ * @s: char *
+ * Return: the len.
+ */
+int _strlen(const char *s)
+{
+        int len = 0;
+
+        while (s[len] != '\0')
+        {
+                len++;
+        }
+        return (len);
+}
+
 /**
  * add_node - Add a new node(s) at the head of a string
  * @head: the first node of a string
