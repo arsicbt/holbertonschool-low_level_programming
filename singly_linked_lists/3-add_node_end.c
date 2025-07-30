56290b8d7701d6aa55eab4e7ab6c4faa40cfeafd
arsicbt <chobertarsinoe@gmail.com>
2025-07-30 08:50:20 +0000

    fix: remove recursion

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -26,31 +26,35 @@ int _strlen(const char *s)
 **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *temp;
 	char *dup;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = dup;
+	new_node->len = _strlen(dup);
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
-		new_node = malloc(sizeof(list_t));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-
-		dup = strdup(str);
-		if (dup == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-
-		new_node->str = dup;
-		new_node->len = _strlen(dup);
-		new_node->next = NULL;
-
 		*head = new_node;
 		return (new_node);
 	}
 
-	return (add_node_end(&((*head)->next), str));
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new_node;
+
+	return (new_node);
 }
--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -8,13 +8,13 @@
 **/
 void free_list(list_t *head)
 {
-	list_t *to_free;
+	list_t *temp;
 
 	while (head != NULL)
 	{
-		to_free = head;
+		temp = head;
 		head = head->next;
-		free(to_free->str);
-		free(to_free);
+		free(temp->str);
+		free(temp);
 	}
 }
