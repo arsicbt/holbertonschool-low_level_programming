affe971afd1f7a3895dac3530d6792f18d15a3c0
arsicbt <chobertarsinoe@gmail.com>
2025-07-30 14:26:05 +0000

    fix: Betty coding style

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -41,5 +41,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current_node->next->prev = new_node;
 	current_node->next = new_node;
 
-	return(new_node);
+	return (new_node);
 }
