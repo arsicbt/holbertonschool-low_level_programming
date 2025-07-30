5301463c86783322b6a9ba1b13a764dfa6550821
arsicbt <chobertarsinoe@gmail.com>
2025-07-30 09:06:06 +0000

    fix: add the len funtcion in 0-print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,6 +3,25 @@
 #include <stddef.h>
 
 /**
+ * _strlen_recursion - Returns the length of a string using recursion
+ * @s: The string to calculate the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*s != '\0')
+	{
+		i += 1;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
+
+/**
  * print_list - Print all element of a list_t
  * @h: Pointer to the end of the list
  * Return: The number of nodes.
@@ -21,7 +40,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n", _strlen(h->str), h->str);
+			printf("[%d] %s\n", _strlen_recursion(h->str), h->str);
 		}
 		h = h->next;
 		len++;
