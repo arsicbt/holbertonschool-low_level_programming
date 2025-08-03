6ac5fa39dbcb640c3206f52f31eac0a8904b8d87
arsicbt <chobertarsinoe@gmail.com>
2025-08-03 18:16:57 +0000

    fix: main function has more than 40 lines

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,32 +4,36 @@
 #include <unistd.h>
 #define BUFFER_SIZE 1024
 
+void error_exit(int code, const char *message, const char *arg)
+{
+	dprintf(STDERR_FILENO, message, arg);
+	exit(code);
+}
+
 /**
+ * main - copie the content of a file to another
+ * @argc: Nummber of arguments
+ * @argv: Argument vector
+ * Return: 0 on succes.
 **/
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r_bytes, w_bytes;
+	int fd_from, fd_to;
+	ssize_t r_bytes, w_bytes;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_exit(97, "Usage: cp file_from file_to\n", "");
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
-		exit(99);
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
 
 	while ((r_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
@@ -37,32 +41,24 @@ int main(int argc, char *argv[])
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes != r_bytes)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
 
 	if (r_bytes == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(98);
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
 
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
 
 	return (0);
 }
