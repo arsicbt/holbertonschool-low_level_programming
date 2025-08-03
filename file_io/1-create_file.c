6ba1520e5f52d613ea3c3e165b7d78e80ff64315
arsicbt <chobertarsinoe@gmail.com>
2025-08-03 16:48:33 +0000

    fix: add library, betty commit, main file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
+/**
+ * create_file - Create a file
+ * @filename: name of the file
+ * @text_content: the text to add
+ * Return: 1 on success.
+**/
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
