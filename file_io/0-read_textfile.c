5898dbb22d81c4c194f8aa4068b3857eadd93e2e
arsicbt <chobertarsinoe@gmail.com>
2025-08-03 16:37:45 +0000

    fix: betty coding style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,9 +5,10 @@
 #include <fcntl.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it standard output.
  * @filename: name of the file
- * @letters: 
+ * @letters: letters to read and print
+ * Return: the number of prints and reads letters.
 **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
